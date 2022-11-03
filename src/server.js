d1b3c7d03106da199550f264a2d0408ba52e0c77
zibrill <zibril101@gmail.com>
2022-11-03 22:18:06 +0700

    fix bug host value for production

--- src/server.js
+++ src/server.js
@@ -2,20 +2,21 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.36.19',
-        routes: {
-            cors: {
-                origin: ['*']
-            }
-        }
-    });
+  const server = Hapi.server({
+    port: 5000,
+    // 172.31.36.19 -> old
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  server.route(routes);
 
-    server.route(routes);
-    await server.start();
+  await server.start();
 
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
